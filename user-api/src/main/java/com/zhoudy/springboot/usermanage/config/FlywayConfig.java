30777896f696a39eedfd9ae3735999cc43f25653
zhoudy-github <781182569@qq.com>
2020-02-11 13:59:09 +0800

    集成flyway

---
+++ user-api/src/main/java/com/zhoudy/springboot/usermanage/config/FlywayConfig.java
@@ -0,0 +1,24 @@
+package com.zhoudy.springboot.usermanage.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+    @Autowired
+    private DataSource dataSource;
+
+    @PostConstruct
+    public void migrate(){
+        Flyway flyway=Flyway.configure()
+                .dataSource(dataSource)
+                .locations("db/migration")
+                .baselineOnMigrate(true)
+                .load();
+        flyway.migrate();
+    }
+}
---
+++ user-api/src/main/resources/db/migration/V1.3__persion.sql
@@ -0,0 +1,3 @@
+use springboot;
+
+insert into person (first,last,dateofbirth,placeofbirth) values('周','东阳', STR_TO_DATE('02/10/1998', '%m/%d/%Y'),'Hello');
