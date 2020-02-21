3b57a079c0f85423fe35c74b9e3620585f85db3e
zhoudy-github <781182569@qq.com>
2020-02-21 17:54:38 +0800

    新增多数据源，优化结构

---
+++ dydb-app/src/main/java/com/zhoudy/dynamicdatasource/dydbapp/DydbAppApplication.java
@@ -0,0 +1,13 @@
+package com.zhoudy.dynamicdatasource.dydbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DydbAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DydbAppApplication.class, args);
+    }
+
+}
